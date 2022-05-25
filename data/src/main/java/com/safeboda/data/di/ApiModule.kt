0a13761e73d1a7ce91b948d5326484816f6d7af5
Peter Munyao <pmunyao63@gmail.com>
2022-05-25 19:11:55 +0300

    Add installIn for apiModule

--- data/src/main/java/com/safeboda/data/di/ApiModule.kt
+++ data/src/main/java/com/safeboda/data/di/ApiModule.kt
@@ -3,9 +3,12 @@ package com.safeboda.data.di
 import com.safeboda.data.apiservice.ApiService
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 
 @Module(includes = [NetworkModule::class])
+@InstallIn(SingletonComponent::class)
 class ApiModule {
 
     @Provides
